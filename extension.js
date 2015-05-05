8c0f21e1e821a8f935626bc803fab2a4d8fa96e2
MarcosXJ6 <dsvilla@outlook.com>
2015-05-05 00:20:07 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -22,15 +22,15 @@
             }
         };
         
-        bot.commands.lennyCommand = {
-            command: 'lenny',  //The command to be called. With the standard command literal this would be: !bacon
+        bot.commands.humCommand = {
+            command: 'hum',  //The command to be called. With the standard command literal this would be: !bacon
             rank: 'user', //Minimum user permission to use the command
             type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
             functionality: function (chat, cmd) {
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
-                    API.sendChat(" [@" + chat.un + "] ( ͡° ͜ʖ ͡°)");
+                    API.sendChat("/me ( ͡° ͜ʖ ͡°)");
                 }
             }
         };
