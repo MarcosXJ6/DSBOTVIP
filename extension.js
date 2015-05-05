d75b212f47358ba3fa14ebf4f08b46efe6e78766
MarcosXJ6 <dsvilla@outlook.com>
2015-05-05 00:31:00 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,121 +1,121 @@
 (function () {
-
+    //Nome de usuário de onde foi feito a cópia, assim você não precisa mudar tanta coisa.
+    var fork = "motelbible";
+		
+    //Definir função pela extensão do bot.
     function extend() {
-            if (!window.bot) {
+        //Se o bot não iniciou corretamente, tente novamente 1 segundo depois.
+        if (!window.bot) {
             return setTimeout(extend, 1 * 1000);
         }
 
+        //Precaução para ter certeza de que foi atribuido corretamente.
         var bot = window.bot;
 
+        //Carregar configurações personalizadas abaixo. ↓
         bot.retrieveSettings();
-        
-        bot.commands.baconCommand = {
-            command: 'bacon',  //The command to be called. With the standard command literal this would be: !bacon
-            rank: 'user', //Minimum user permission to use the command
-            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
-            functionality: function (chat, cmd) {
-                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-                if (!bot.commands.executable(this.rank, chat)) return void (0);
-                else {
-                    API.sendChat("/me Bacon!! :pig2:");
-                }
-            }
-        };
-        
-        bot.commands.humCommand = {
-            command: 'hum',  //The command to be called. With the standard command literal this would be: !bacon
-            rank: 'user', //Minimum user permission to use the command
-            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
-            functionality: function (chat, cmd) {
-                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-                if (!bot.commands.executable(this.rank, chat)) return void (0);
-                else {
-                    API.sendChat("/me ( ͡° ͜ʖ ͡°)");
-                }
-            }
-        };
-        
-        bot.commands.origemCommand = {
-            command: 'origem',  //The command to be called. With the standard command literal this would be: !bacon
-            rank: 'user', //Minimum user permission to use the command
-            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
-            functionality: function (chat, cmd) {
-                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-                if (!bot.commands.executable(this.rank, chat)) return void (0);
-                else {
-                    API.sendChat("/me Recomendamos que use o OrigemWoot, veja um tutorial de como usa-lo. http://origemwoot.weebly.com/ :+1:");
-                }
-            }
-        };
 
+        /*
         bot.commands.cookieCommand.cookies =['deu-lhe um biscoito de chocolate!',
-                    'Quer te fuder até o talo!',
-                    'deu-lhe um biscoito que está escrito "você é um viado"',
-                    'deu-lhe um biscoito da sorte, tem escrito: "Vou te dar um ban eterno de 30 dias"',
-                    'deu-lhe um biscoito da sorte, tem escrito: "Mostra as teta ae :trollface:"',
-                    'deu-lhe um cookie vencido!"',
-                    'deu-lhe um biscoito da sorte, tem escrito: "Você é um fracassado!"',
-                    'deu-lhe um biscoito da sorte, tem escrito: "Se você mecher o quadril, vão achar que você é gay!',
-                    'deu-lhe um biscoito da sorte, tem escrito: "Eu te amo *-*"',
-                    'deu-lhe um Trakinas e um copo de refrigerante',
+                    'deu-lhe um biscoito de aveia caseiro macio!',
+                    'deu-lhe um biscoito podre e sujo. Era o último do pacote. Que nojo!',
+                    'deu-lhe um bolinho de açúcar... O quê? Sem estrelinhas e povilho? Eu não tocaria.',
+                    'deu-lhe um biscoito de chocolate. Oh, não, são passas. Eca!',
+                    'deu-lhe um enorme biscoito. Quando o toca, ele se duplica num outro biscoito... estranho',
+                    'deu-lhe um biscoito da sorte, tem escrito: "Por que você não está trabalhando?"',
+                    'deu-lhe um biscoito da sorte, tem escrito: "Cumprimente agora a pessoa que você ama"',
+                    'deu-lhe um biscoito da sorte, tem escrito: "Arrisque-se!"',
+                    'deu-lhe um biscoito da sorte, tem escrito: "Saia desse computador!"',
+                    'deu-lhe um biscoito da sorte, tem escrito: "Não esqueça de comer os vegetais"',
+                    'deu-lhe um biscoito da sorte, tem escrito: "Se você mecher o quadril, vão te achar sexy!',
+                    'deu-lhe um biscoito da sorte, tem escrito: "Eu te amo"',
+                    'deu-lhe um biscoito de ouro, mas não dá pra comer... Droga!',
+                    'deu-lhe um Oreo e um copo de leite.',
                     'deu-lhe um biscoito de arco-íris feito com amor :heart:',
                     'deu-lhe um biscoito que foi esquecido na chuva... eu não comeria.',
-                    'te trouxe biscoitos fresquinhos... parecem deliciosos! :3'
+                    'te trouxe biscoitos fresquinhos... parecem deliciosos!'
                 ];
+        */
+        
+        bot.commands.testCommand = {
+            command: ['test'], //Exemplo de comando
+            rank: 'user', //user/bouncer/mod/manager
+            type: 'exact',
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("/me 1, 2, 3... testando"); // Mensagem a ser enviada no chat
+                }
+            }
+        };
 
+        //Carregar o pacote do chat novamente para registrar todas as mudanças
         bot.loadChat();
+
     }
 
-    
+    //Mudar as configurações padrões do bot e ter certeza que elas foram carregadas corretamente
+
     localStorage.setItem("basicBotsettings", JSON.stringify({
-        botName: "Ds (SunriseBOT)",
+        botName: "Ds (BOT)",
         language: "portuguese",
-        chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/pt.json",
+        startupCap: 1, // 1-200
+        startupVolume: 0, // 0-100
+        startupEmoji: false, // true ou false
+        chatLink: "https://rawgit.com/" + fork + "/basicBot/master/lang/pt.json",
         maximumAfk: 120,
-        afkRemoval: false,
+        afkRemoval: true,
         maximumDc: 60,
-        bouncerPlus: false,
+        bouncerPlus: true,
+        blacklistEnabled: true,
         lockdownEnabled: false,
         lockGuard: false,
         maximumLocktime: 10,
         cycleGuard: true,
         maximumCycletime: 10,
+        voteSkip: false,
+        voteSkipLimit: 10,
         timeGuard: true,
-        maximumSongLength: 7,
+        maximumSongLength: 10,
         autodisable: true,
         commandCooldown: 30,
         usercommandsEnabled: true,
-        lockskipPosition: 1,
+        lockskipPosition: 3,
         lockskipReasons: [
-            ["tema", "A música não se encaixa nos padrões da sala. "],
-                ["op", "Essa música está na lista OP. "],
-                ["historico", "A música ainda está no histórico. "],
-                ["mix", "Você tocou um mix (muito longo) - não permitido. "],
-                ["som", "A música que você tocou tinha má qualidade ou estava sem som. "],
-                ["nsfw", "A música que você tocou é NSFW (impróprio). "],
-                ["indisponivel", "A música que você tocou está indisponível. "]
+        	["tema", "A música não se encaixa nos padrões da sala. "],
+        	["op", "Essa música está na lista OP. "],
+        	["historico", "A música ainda está no histórico. "],
+        	["mix", "Você tocou um mix (muito longo) - não permitido. "],
+        	["som", "A música que você tocou tinha má qualidade ou estava sem som. "],
+        	["nsfw", "A música que você tocou é NSFW (impróprio). "],
+        	["indisponivel", "A música que você tocou está indisponível. "]
         ],
         afkpositionCheck: 15,
         afkRankCheck: "ambassador",
         motdEnabled: false,
-        motdInterval: 3,
+        motdInterval: 5,
         motd: "Mensagem temporária do dia",
-        filterChat: false,
-        etaRestriction: true,
-        welcome: false,
+        filterChat: true,
+        etaRestriction: false,
+        welcome: true,
         opLink: null,
         rulesLink: null,
         themeLink: null,
-        fbLink: "https://facebook.com/groups/1497216277163895/",
+        fbLink: null,
         youtubeLink: null,
-        website: "",
+        website: null,
         intervalMessages: [],
         messageInterval: 5,
-        songstats: false,
-        commandLiteral: "!"
+        songstats: true,
+        commandLiteral: "!",
+        blacklists: {
+            NSFW: "https://rawgit.com/" + fork + "/basicBot-customization/master/blacklists/ExampleNSFWlist.json",
+            OP: "https://rawgit.com/" + fork + "/basicBot-customization/master/blacklists/ExampleOPlist.json"
+        }
     }));
 
-    //Start the bot and extend it when it has loaded.
-    $.getScript('https://rawgit.com/Yemasthui/basicBot/master/basicBot.js', extend);
-    
+    //Inicia o bot e o extende quando for completamente carregado.
+    $.getScript("https://rawgit.com/motelbible/basicBot/master/basicBot.js", extend);
+
 }).call(this);
